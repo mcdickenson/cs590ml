d3d2a776d4409451fd1c0006a7e285abf316e81e
Matt Dickenson <mcdickenson@gmail.com>
2014-04-13 13:38:09 -0400

    add poster directory

---
+++ poster/plot.R
@@ -0,0 +1,32 @@
+plot.R
+
+rm(list=ls())
+setwd('~/github/olympifier')
+library(evtree)
+library(mclust)
+library(nnet)
+library(party)
+library(RColorBrewer)
+library(randomForest)
+
+
+# load data
+athletes = read.csv('data/athletes-clean.csv', header=TRUE, as.is=TRUE)
+dim(athletes)
+head(athletes)
+athletes = athletes[-which(athletes$Sport=="Athletics"),]
+athletes$sport.factor = as.factor(athletes$Sport)
+athletes$event.factor = as.factor(athletes$FirstEvent)
+length(unique(athletes$sport.factor))
+sort(unique(athletes$sport.factor))
+
+
+# set up training and test sets
+features = c("Age", "Height", "Weight", "Female")
+target = "sport.factor"
+summary(athletes$kfold)
+trnData = athletes[athletes$kfold <= 5, features]
+tstData = athletes[athletes$kfold >  5, features]
+trnClass = athletes[athletes$kfold <= 5, target]
+tstClass = athletes[athletes$kfold >  5, target]
+dim(trnData)
